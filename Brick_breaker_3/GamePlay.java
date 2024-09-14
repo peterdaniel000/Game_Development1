b157813db951be82dfedb3401543734742739864
Daniel Peter <peterdanielfun24@gmail.com>
2024-09-14 16:18:25 +0100

    my first game

--- Brick_breaker_3/GamePlay.java
+++ Brick_breaker_3/GamePlay.java
@@ -1,5 +1,170 @@
 package Brick_breaker_3;
 
-public class GamePlay {
+import javax.swing.JPanel;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener{
+    
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBricks = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    private MapGenerator map;
+
+    public GamePlay(){
+        map = new MapGenerator(3, 7);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+        this.requestFocusInWindow(); 
+    }
+    public void paint(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(1,1,692,592);
+
+        map.draw((Graphics2D)g);
+        
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3,592);
+
+        g.setColor(Color.blue);
+        g.fillRect(playerX, 550, 100, 8);
+
+        g.setColor(Color.green);
+        g.fillOval(ballposX , ballposY, 20, 20);
+
+        g.dispose();
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        timer.start();
+
+        if (play) {
+            if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))){
+                ballYdir = -ballYdir;
+            }
+                A:
+            for (int i = 0; i <map.map.length; i++){
+                for (int j = 0; j < map.map[0].length; j++){
+                    if(map.map[i][j] > 0) {
+                        int brickX = j*map.BrickWidth + 80;
+                        int brickY = i*map.BrickHeight + 50;
+                        int BrickWidth = map.BrickWidth;
+                        int BrickHeight = map.BrickHeight;
+
+                        Rectangle rect = new Rectangle(brickX,brickY,BrickWidth,BrickHeight);
+                        Rectangle ballRect = new Rectangle(ballposX,ballposY,20,20);
+                        Rectangle brickRect = rect;
+
+                        if(ballRect.intersects(brickRect)) {
+                            map.setBrickValue(0, i, j);
+                            totalBricks--;
+                            score+=5;
+
+                            if (ballposX + 19 <= brickRect.x || ballposY +1 >= brickRect.x + brickRect.width){
+                                ballXdir = -ballXdir;
+                            }else {
+                                ballYdir = -ballYdir;
+                            }
+                            break A; // Break out of both loops
+                        }
+                    }
+                }
+            }
+            
+
+            ballposX += ballXdir;
+            ballposY += ballYdir;
+            if ( ballposX < 0){
+                ballXdir = -ballXdir;
+            }
+            if ( ballposY < 0){
+                ballYdir = -ballYdir;
+            }
+            if ( ballposX > 670){
+                ballXdir = -ballXdir;
+            }
+
+
+
+        }
+
+        repaint();
+        //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if (playerX >= 600){
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        }
+
+
+
+        if (e.getKeyCode() == KeyEvent.VK_LEFT){
+            if (playerX < 10){
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+
+        }
+
+        }
+
+        public void moveLeft(){
+            play = true;
+            playerX -= 20;
+        }
+            public void moveRight(){
+                play = true;
+                playerX += 20;
+            }
+        //throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+        //throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
     
 }
--- Brick_breaker_3/MapGenerator.java
+++ Brick_breaker_3/MapGenerator.java
@@ -1,5 +1,52 @@
 package Brick_breaker_3;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.Color;
 
 public class MapGenerator {
     
-}
+    public int map[][];
+    public int BrickWidth;
+    public int BrickHeight;
+
+    public MapGenerator(int row, int col) {
+        map = new int[row][col];
+
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map[0].length; j++){
+                map[i][j] = 1;
+            }
+        }
+    
+       BrickWidth = 540/col;
+       BrickHeight = 150/row;
+    }
+
+           public void draw(Graphics2D g){
+            for (int i = 0; i < map.length; i++){
+                for (int j = 0; j < map[0].length; j++) {
+                    if(map[i][j] > 0){
+                        if (map[i][j] > 0){
+                            g.setColor(Color.black);
+                            g.fillRect(j*BrickWidth + 80,i*BrickHeight + 50, BrickWidth, BrickHeight);
+
+                            g.setStroke(new BasicStroke(3));
+                            g.setColor(Color.white);
+                            g.drawRect(j*BrickWidth + 80, i*BrickHeight +50, BrickWidth, BrickHeight);
+                        }
+
+                    }
+                }
+            }
+         
+        
+        }
+
+        public void setBrickValue(int value, int row, int col){
+            map[row][col] = value;
+        }
+         
+
+    }
+
+
